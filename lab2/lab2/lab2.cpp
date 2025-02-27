491b0ae033d5d8e55b7fcd6a3214b50ca5ac7016
Wumpy0 <wumpy0@gmail.com>
2025-02-28 00:10:10 +0800

    lab2: создание

---
+++ lab2/lab2/lab2.cpp
@@ -0,0 +1,7 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::string text = { "std::move_iterator is an iterator adaptor which behaves exactly like the underlying iterator" };
+	std::string pattern = { "tor" };
+}
