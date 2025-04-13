fedd755a08735948f1464a54b2a16711b2ba927e
Wumpy0 <wumpy0@gmail.com>
2025-04-14 04:16:19 +0800

    lab3: Созданы файлы лабораторной, реализована функция проверки массива на упорядоченность

---
+++ lab3/lab3/lab3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <random>
+
+bool isSorted(const std::vector<int>& vector) {
+	return std::is_sorted(vector.begin(), vector.end());
+}
+
+int main()
+{
+	std::vector<int> vector = { 1, 2, 3, 5, 7, 98 };
+
+	std::cout << isSorted(vector);
+	std::cout << RAND_MAX;
+}
