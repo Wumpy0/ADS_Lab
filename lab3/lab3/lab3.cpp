534310c0fde123b4f95b9a978ef96c8849749974
Wumpy0 <wumpy0@gmail.com>
2025-04-16 01:22:25 +0800

    lab3: реализованы алгоритмы сортировки Шелла с выбором промежутков по методу Кнута и Седжвика

--- lab3/lab3/lab3.cpp
+++ lab3/lab3/lab3.cpp
@@ -50,10 +50,62 @@ void shellSort(std::vector<int>& vector) {
     }
 }
 
+void shellSortKnuth(std::vector<int>& vector) {
+    int vectorSize = vector.size();
+
+    int gap = 1;
+    while (gap < vectorSize / 3) {
+        gap = 3 * gap + 1;
+    }
+
+    while (gap >= 1) {
+        for (int i = gap; i < vectorSize; i++) {
+            int temp = vector[i];
+            int j;
+            for (j = i; j >= gap && vector[j - gap] > temp; j -= gap) {
+                vector[j] = vector[j - gap];
+            }
+            vector[j] = temp;
+        }
+        gap /= 3;
+    }
+}
+
+void shellSortSedgewick(std::vector<int>& arr) {
+    int n = arr.size();
+
+    std::vector<int> gaps;
+    int k = 0;
+    while (true) {
+        int gap;
+        if (k & 1) {
+            gap = 8 * (1 << k) - 6 * (1 << ((k + 1) / 2)) + 1;
+        }
+        else {
+            gap = 9 * (1 << k) - 9 * (1 << (k / 2)) + 1;
+        }
+        if (gap >= n) break;
+        gaps.push_back(gap);
+        k++;
+    }
+
+    for (int s = gaps.size() - 1; s >= 0; s--) {
+        int gap = gaps[s];
+        for (int i = gap; i < n; i++) {
+            int temp = arr[i];
+            int j;
+            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+                arr[j] = arr[j - gap];
+            }
+            arr[j] = temp;
+        }
+    }
+}
+
 int main()
 {
-    std::vector<int> vector = loadFile("1000000_numbers_in_range_100000.txt");
-    shellSort(vector);
+    std::vector<int> vector = loadFile("10000_numbers_in_range_100000.txt");
+    shellSortSedgewick(vector);
     //for (int n : vector) {
     //    std::cout << n << " ";
     //}
