724fbc47331968f42259342c86217a62875b253d
Wumpy0 <wumpy0@gmail.com>
2025-04-17 14:31:30 +0800

    lab4: создание, функция проверки сортировки скопирована из lab3

---
+++ lab4/lab4/lab4.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+bool isSorted(const std::vector<int>& vector) {
+	return std::is_sorted(vector.begin(), vector.end());
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
