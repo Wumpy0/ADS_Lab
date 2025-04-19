c58bda84909cc0c50e3f8ec226dceeaba5bd0614
Wumpy0 <wumpy0@gmail.com>
2025-04-19 16:46:47 +0800

    lab5: созданы файлы лабораторной работы, из предыдущих лабораторных скопированы необходимые функции

---
+++ lab5/lab5/lab5.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <fstream>
+#include <chrono>
+#include <iomanip>
+
+bool isSorted(const std::vector<int>& vector) {
+    return std::is_sorted(vector.begin(), vector.end());
+}
+
+std::vector<int> loadFile(const std::string& filename) {
+    std::ifstream in(filename);
+    std::vector<int> vector;
+    int number;
+    while (in >> number) {
+        vector.push_back(number);
+    }
+    return vector;
+}
+
+void quickSort(std::vector<int>& vector) {
+
+}
+
+int main()
+{
+    std::vector<int> vector = loadFile("10000_numbers_in_range_10.txt");
+    quickSort(vector);
+    std::cout << isSorted(vector);
+}
