10ce5d3f9605defe2c8d45504cf6c1766227de78
Wumpy0 <wumpy0@gmail.com>
2025-04-20 23:17:21 +0800

    lab6: созданы файлы лабораторной работы, из предыдущей лабораторной работы скопированы функции загрузки массива из файла и проверки массива на отсортированность. Часть кода для тестирования на 9 файлах так же взята из предыдущей лабораторной работы. Реализована вспомогательная функция isBit для проверки, установлен ли k-й бит в числе num. Реализована рекурсивная функция побитовой сотировки bitwiseSort

---
+++ lab6/lab6/lab6.cpp
@@ -0,0 +1,115 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <fstream>
+#include <chrono>
+#include <iomanip>
+
+bool isSorted(const std::vector<int>& vector) {
+    return std::is_sorted(vector.begin(), vector.end());
+}
+
+std::vector<int> loadFile(const std::string& filename) {
+    std::ifstream in(filename);
+    std::vector<int> vector;
+    int number;
+    while (in >> number) {
+        vector.push_back(number);
+    }
+    return vector;
+}
+
+bool isBit(int num, int k) {
+    return (num & (1 << k)) != 0;
+}
+
+void bitwiseSort(std::vector<int>& vector, int left, int right, int bit) {
+    if (left >= right || bit < 0) {
+        return;
+    }
+
+    int i = left;
+    int j = right;
+
+    if (bit == 31) {
+        while (i <= j) {
+            while (i <= j && isBit(vector[i], bit)) {
+                i++;
+            }
+            while (i <= j && !isBit(vector[j], bit)) {
+                j--;
+            }
+            if (i < j) {
+                std::swap(vector[i], vector[j]);
+                i++;
+                j--;
+            }
+        }
+    }
+    else {
+        while (i <= j) {
+            while (i <= j && !isBit(vector[i], bit)) {
+                i++;
+            }
+            while (i <= j && isBit(vector[j], bit)) {
+                j--;
+            }
+            if (i < j) {
+                std::swap(vector[i], vector[j]);
+                i++;
+                j--;
+            }
+        }
+    }
+
+    bitwiseSort(vector, left, j, bit - 1);
+    bitwiseSort(vector, i, right, bit - 1);
+}
+
+int main() {
+    std::vector<std::string> filenames = {
+    "10000_numbers_in_range_10.txt",
+    "10000_numbers_in_range_1000.txt",
+    "10000_numbers_in_range_100000.txt",
+    "100000_numbers_in_range_10.txt",
+    "100000_numbers_in_range_1000.txt",
+    "100000_numbers_in_range_100000.txt",
+    "1000000_numbers_in_range_10.txt",
+    "1000000_numbers_in_range_1000.txt",
+    "1000000_numbers_in_range_100000.txt"
+    };
+
+    const int runs = 3;
+    std::ofstream resultsFile("results.txt");
+    resultsFile << "Filename, Algorithm, Average (s), Sorted Correctly\n";
+    resultsFile << std::fixed << std::setprecision(6);
+
+    for (const auto& filename : filenames) {
+        std::cout << "Testing file: " << filename << std::endl;
+        auto originalData = loadFile(filename);
+
+        std::vector<double> times;
+        bool allCorrect = true;
+
+        for (int i = 0; i < runs; ++i) {
+            auto data = originalData;
+            auto start = std::chrono::high_resolution_clock::now();
+            bitwiseSort(data, 0, data.size() - 1, 31);
+            auto end = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double> duration = end - start;
+            times.push_back(duration.count());
+
+            if (!isSorted(data)) {
+                allCorrect = false;
+            }
+        }
+
+        double average = (times[0] + times[1] + times[2]) / runs;
+        resultsFile << filename << ", bitwiseSort, " << average << ", " << (allCorrect ? "Yes" : "No") << "\n";
+    }
+
+    resultsFile.close();
+    std::cout << "All tests completed!" << std::endl;
+
+    return 0;
+}
