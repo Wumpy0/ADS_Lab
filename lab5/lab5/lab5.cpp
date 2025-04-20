4f5579b14ab973e9533b998e00144af29dbcd1e2
Wumpy0 <wumpy0@gmail.com>
2025-04-20 17:53:51 +0800

    lab5: реализована функция сортировки quickSort, часть кода для тестирования на 9 файлах взята из предыдущей лабораторной работы

--- lab5/lab5/lab5.cpp
+++ lab5/lab5/lab5.cpp
@@ -19,13 +19,80 @@ std::vector<int> loadFile(const std::string& filename) {
     return vector;
 }
 
-void quickSort(std::vector<int>& vector) {
+void quickSort(std::vector<int>& vector, int leftBound, int rightBound) {
+    if (leftBound >= rightBound) {
+        return;
+    }
 
+    int i = leftBound;
+    int j = rightBound;
+    int central = vector[(leftBound + rightBound) / 2];
+
+    while (i <= j) {
+        while (vector[i] < central) {
+            i++;
+        }
+
+        while (vector[j] > central) {
+            j--;
+        }
+
+        if (i <= j) {
+            std::swap(vector[i], vector[j]);
+            i++;
+            j--;
+        }
+    }
+
+    quickSort(vector, leftBound, j);
+    quickSort(vector, i, rightBound);
 }
 
 int main()
 {
-    std::vector<int> vector = loadFile("10000_numbers_in_range_10.txt");
-    quickSort(vector);
-    std::cout << isSorted(vector);
+    std::vector<std::string> filenames = {
+    "10000_numbers_in_range_10.txt",
+    "10000_numbers_in_range_1000.txt",
+    "10000_numbers_in_range_100000.txt",
+    "100000_numbers_in_range_10.txt",
+    "100000_numbers_in_range_1000.txt",
+    "100000_numbers_in_range_100000.txt",
+    "1000000_numbers_in_range_10.txt",
+    "1000000_numbers_in_range_1000.txt",
+    "1000000_numbers_in_range_100000.txt"
+    };
+
+    const int runs = 3;
+    std::ofstream resultsFile("results.txt");
+    resultsFile << "Filename, Algorithm, Average (s), Sorted Correctly\n";
+    resultsFile << std::fixed << std::setprecision(6);
+
+    for (const auto& filename : filenames) {
+        std::cout << "Testing file: " << filename << std::endl;
+        auto originalData = loadFile(filename);
+
+        std::vector<double> times;
+        bool allCorrect = true;
+
+        for (int i = 0; i < runs; ++i) {
+            auto data = originalData;
+            auto start = std::chrono::high_resolution_clock::now();
+            quickSort(data, 0, data.size() - 1);
+            auto end = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double> duration = end - start;
+            times.push_back(duration.count());
+
+            if (!isSorted(data)) {
+                allCorrect = false;
+            }
+        }
+
+        double average = (times[0] + times[1] + times[2]) / runs;
+        resultsFile << filename << ", quickSort, " << average << ", " << (allCorrect ? "Yes" : "No") << "\n";
+    }
+
+    resultsFile.close();
+    std::cout << "All tests completed!" << std::endl;
+
+    return 0;
 }
